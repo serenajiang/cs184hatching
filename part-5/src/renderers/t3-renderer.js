5a22558ce3dac4f53b8fa71115c2c19319adba6d
Serena Jiang <serenajiang@berkeley.edu>
2018-04-15 16:57:41 -0700

    more TAMs

--- part-5/src/renderers/t3-renderer.js
+++ part-5/src/renderers/t3-renderer.js
@@ -4,8 +4,11 @@ import Renderer from '../lib/renderer';
 import vert from '../shaders/task-3/texture.vert';
 import frag from '../shaders/task-3/texture.frag';
 
-import texture2 from '../textures/haha43.bmp';
-import texture1 from '../textures/haha13.bmp';
+import texture5 from '../textures/test/ayy53.bmp';
+import texture4 from '../textures/test/ayy43.bmp';
+import texture3 from '../textures/test/ayy33.bmp';
+import texture2 from '../textures/test/ayy23.bmp';
+import texture1 from '../textures/test/ayy13.bmp';
 
 export default class extends Renderer {
   initScene() {
@@ -14,8 +17,23 @@ export default class extends Renderer {
       return;
     }
     this.setLight();
-    var tex2 = new THREE.TextureLoader().load(texture2);
+    var tex5 = new THREE.TextureLoader().load(texture5);
+    this.uniforms['texture53'] = {
+      type: "t",
+      value: tex5//new THREE.TextureLoader().load(texture)
+    };
+    var tex4 = new THREE.TextureLoader().load(texture4);
     this.uniforms['texture43'] = {
+      type: "t",
+      value: tex4//new THREE.TextureLoader().load(texture)
+    };
+    var tex3 = new THREE.TextureLoader().load(texture3);
+    this.uniforms['texture33'] = {
+      type: "t",
+      value: tex3//new THREE.TextureLoader().load(texture)
+    };
+    var tex2 = new THREE.TextureLoader().load(texture2);
+    this.uniforms['texture23'] = {
       type: "t",
       value: tex2//new THREE.TextureLoader().load(texture)
     };
@@ -25,7 +43,8 @@ export default class extends Renderer {
       value: tex1//new THREE.TextureLoader().load(texture)
     };
 
-    const geometry = new THREE.SphereGeometry(5, 32, 32);
+    //const geometry = new THREE.SphereGeometry(5, 32, 32);
+    const geometry = new THREE.TeapotBufferGeometry(4, 32, 32);
     const material = this.createShaderMaterial(vert, frag);
     const cube = new THREE.Mesh(geometry, material);
     this.scene.add(cube);
--- part-5/src/shaders/task-3/texture.frag
+++ part-5/src/shaders/task-3/texture.frag
@@ -1,7 +1,10 @@
 precision highp float;
 
 uniform sampler2D texture13;
+uniform sampler2D texture23;
+uniform sampler2D texture33;
 uniform sampler2D texture43;
+uniform sampler2D texture53;
 varying vec2 fUv;
 
 uniform vec3 lPosition;
@@ -17,11 +20,17 @@ void main() {
   vec3 color = vec3(0.3,0.3,0.3)
   + 0.6 * lIntensity/r2 * max(0.0, dot(normalize(fNormal), normalize(lPosition)))
   + 0.4 * lIntensity/r2 * pow(max(0.0, dot(normalize(fNormal), h)), 50.0);
-
-  if (dot(color, vec3(1.,0.,0.)) > 0.) {
-    gl_FragColor = texture2D(texture13, fUv);
-  } else {
+  float brightness = dot(color, vec3(1.,0.,0.));
+  if (brightness < .2) {
+    gl_FragColor = texture2D(texture53, fUv);
+  } else if (brightness < .4){
     gl_FragColor = texture2D(texture43, fUv);
+  } else if (brightness < .6){
+    gl_FragColor = texture2D(texture33, fUv);
+  } else if (brightness < .8){
+    gl_FragColor = texture2D(texture23, fUv);
+  } else {
+    gl_FragColor = texture2D(texture13, fUv);
   }
 
 }
